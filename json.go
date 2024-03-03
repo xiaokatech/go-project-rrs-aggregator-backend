bc4c0273a612cd75d7382b41234b876f3a077155
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-03-04 00:33:59 +0100

    Apply handler_err

--- json.go
+++ json.go
@@ -6,6 +6,21 @@ import (
 	"net/http"
 )
 
+func responseWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Println("Responding with 5XX error:", msg)
+	}
+
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	// responseWithJSON(w, code, errResponse{msg}) // Good, but not explicitly show field
+	responseWithJSON(w, code, errResponse{
+		Error: msg,
+	})
+}
+
 func responseWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 	if err != nil {
