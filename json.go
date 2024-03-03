25adde29462b1d335e6f900e87b74decef79efbd
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-03-04 00:26:21 +0100

    Add /v1/healthz

--- json.go
+++ json.go
@@ -14,6 +14,6 @@ func responseWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.Header().Add("Content-Type", "application/json")
-	w.WriteHeader(200)
+	w.WriteHeader(code)
 	w.Write(dat)
 }
