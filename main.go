b6c7faa0ba029b9861523231d64b6f70b8ddf819
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-03-03 23:58:24 +0100

    Update env

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	// Need to run `export PORT=8000`
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port:", portString)
 }
