359b8cdea042e80f572c730d4d096bfc33e50205
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-03-04 16:14:06 +0100

    Update schema and queries for database

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -3,6 +3,17 @@ INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
 VALUES ($1, $2, $3, $4, $5, $6)
 RETURNING *;
 
-
 -- name: GetFeeds :many
 SELECT * from feeds;
+
+-- name: GetNextFeedToFetch :one
+SELECT * from feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
+
+-- name: MarkFeedAsFetched :one
+UPDATE feeds
+SET last_feched_at = NOW(),
+updated_at = NOW()
+WHERE id = $1
+RETURNING *;
