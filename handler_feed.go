83946dd798f263f779c0ceab50cc055c9172fcf0
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-03-04 14:50:04 +0100

    Add POST /v1/feeds

---
+++ handler_feed.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/xiaokatech/go-project-rrs-aggregator-backend/internal/database"
+)
+
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Err parsing JSON: %v", err))
+		return
+	}
+
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      params.Name,
+		Url:       params.URL,
+		UserID:    user.ID,
+	})
+	if err != nil {
+		responseWithError(w, 400, fmt.Sprintf("Couldn't create feed: %v", err))
+		return
+	}
+
+	responseWithJSON(w, 201, databaseFeedToFeed(feed))
+}
