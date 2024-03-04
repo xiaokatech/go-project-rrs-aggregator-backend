38b3258b73ccf2e57b78d38a132999a139c4d249
Jiean Yang <jiean.yang.freelancer@gmail.com>
2024-03-04 17:15:30 +0100

    Debug and createPost

--- rss.go
+++ rss.go
@@ -21,7 +21,7 @@ type RSSItem struct {
 	Title       string `xml:"title"`
 	Link        string `xml:"link"`
 	Description string `xml:"description"`
-	PubDate     string `xml:""pubDate`
+	PubDate     string `xml:"pubDate"`
 }
 
 func urlToFeed(url string) (RSSFeed, error) {
--- scraper.go
+++ scraper.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"log"
 	"sync"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/xiaokatech/go-project-rrs-aggregator-backend/internal/database"
 )
 
@@ -52,7 +54,34 @@ func scrapFeed(db *database.Queries, wg *sync.WaitGroup, feed database.Feed) {
 	}
 
 	for _, item := range rssFeed.Channel.Item {
-		log.Println("Found post", item.Title, "on feed", feed.Name)
+		log.Println("Found post", item.Title, "publishAt", item.PubDate, "on feed", feed.Name)
+
+		description := sql.NullString{}
+		if item.Description != "" {
+			description.String = item.Description
+			description.Valid = true
+		}
+
+		pubAt, err := time.Parse(time.RFC1123Z, item.PubDate)
+		if err != nil {
+			log.Printf("couldn't parse data %v with err %v", item.PubDate, err)
+			continue
+		}
+
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Description: description,
+			PublishedAt: pubAt,
+			Url:         item.Link,
+			FeedID:      feed.ID,
+		})
+		if err != nil {
+			log.Println("failed to create post:", err)
+		}
+
 	}
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 
